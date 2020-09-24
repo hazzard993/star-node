e671316bf8d95291bfb7d510e477dcc9328d43f3
hazzard993 <hazzard993@gmail.com>
2020-09-24 19:29:22 +1000

    Use copyfiles

--- init.lua
+++ init.lua
@@ -1,23 +1,23 @@
 local directory = ...
 
 package.preload["path"] = function()
-    return require(directory .. ".node_modules.path.path")
+    return require(directory .. ".lib.path")
 end
 
 package.preload["fs_common"] = function()
-    return require(directory .. ".node_modules.fs.fs_common")
+    return require(directory .. ".lib.fs_common")
 end
 
 package.preload["fs"] = function()
-    return require(directory .. ".node_modules.fs.fs")
+    return require(directory .. ".lib.fs")
 end
 
 package.preload["fs_posix"] = function()
-    return require(directory .. ".node_modules.fs.fs_posix")
+    return require(directory .. ".lib.fs_posix")
 end
 
 package.preload["fs_win"] = function()
-    return require(directory .. ".node_modules.fs.fs_win")
+    return require(directory .. ".lib.fs_win")
 end
 
 require(directory .. ".lnr")
