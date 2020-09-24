3cfc1a424dc5233bc9fcfefd4cb131a053ad4b27
hazzard993 <hazzard993@gmail.com>
2020-09-24 19:13:18 +1000

    Add luapower dependencies

---
+++ init.lua
@@ -0,0 +1,23 @@
+local directory = ...
+
+package.preload["path"] = function()
+    return require(directory .. ".node_modules.path.path")
+end
+
+package.preload["fs_common"] = function()
+    return require(directory .. ".node_modules.fs.fs_common")
+end
+
+package.preload["fs"] = function()
+    return require(directory .. ".node_modules.fs.fs")
+end
+
+package.preload["fs_posix"] = function()
+    return require(directory .. ".node_modules.fs.fs_posix")
+end
+
+package.preload["fs_win"] = function()
+    return require(directory .. ".node_modules.fs.fs_win")
+end
+
+require(directory .. ".node-resolver")
